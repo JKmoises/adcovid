112c149a5d49b0d38906072476fa065cb71fd95a
JKmoises <moi.prado@duocuc.cl>
2023-02-01 17:09:17 -0300

    refactorizando a version más actual el sistema de enrutamiento e React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Continentes } from './pages/Continentes';
+import { Globales } from './pages/Globales';
+import { Pais } from './pages/Pais';
+import { Vacunas } from './pages/Vacunas';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Globales />,
+
+      },
+      {
+        path: "/casos-continente",
+        element: <Continentes />,
+      },
+      {
+        path: "/casos-pais",
+        element: <Pais />,
+      },
+      {
+        path: "/vacunas",
+        element: <Vacunas />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
